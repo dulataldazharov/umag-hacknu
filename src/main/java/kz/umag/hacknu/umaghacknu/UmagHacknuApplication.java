9ad74469f6b74d7fd3d062718a872394fe48668a
dulataldazharov <dulat.aldazharov@nu.edu.kz>
2023-04-15 19:06:55 +0600

    Init project.

---
+++ src/main/java/kz/umag/hacknu/umaghacknu/UmagHacknuApplication.java
@@ -0,0 +1,13 @@
+package kz.umag.hacknu.umaghacknu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmagHacknuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmagHacknuApplication.class, args);
+    }
+
+}
